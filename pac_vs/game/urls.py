90e57e35aacb403e6dc0317552e7763c0d5ed1e3
kovacikk <kovacikkyl@gmail.com>
2019-04-15 14:17:49 -0400

    Url changes work

--- pac_vs/game/urls.py
+++ pac_vs/game/urls.py
@@ -1,4 +1,4 @@
-"""pac_vs URL Configuration
+"""BigBox URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -13,13 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls import include, url
+from django.urls import path, reverse_lazy
+from django.conf.urls import url, include
+from django.contrib.auth import views as auth_views
+from . import views
 from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import path, include
+from django.conf import settings
+
+#app_name='game'
 
 urlpatterns = [
-    path('', include('game.urls')),
-    path('admin/', admin.site.urls),
+    #home
+	path('', views.home, name='home'),
 ]
